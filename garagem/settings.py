cd659cbcc871743daab0553c9a34903d74cad8c6
gustavotrentinidev <gustavotrentini22@gmail.com>
2022-09-20 16:37:21 -0300

    espaços nas settings

--- garagem/settings.py
+++ garagem/settings.py
@@ -115,6 +115,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 CORS_ALLOW_ALL_ORIGINS = True
+
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
